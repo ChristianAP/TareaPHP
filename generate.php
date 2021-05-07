ac21f8291e63d8c3be447244776e02a4440678a7
Christian <christianatencio@upeu.edu.pe>
2021-05-06 19:47:53 -0500

    Version Beta

---
+++ generate.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    $host = "127.0.0.1";
+    $usuario = "root";
+    $clave = "root";
+    $bd = "tienda";
+    $con = mysqli_connect($host, $usuario, $clave, $bd);
+    if($con -> connect_errno){
+        die("Error de conexión ... !");
+    }
+    $select = mysqli_query($con , "select idcliente, nombres from cliente");
+    $f1 = $_POST['inicio']." 00:00:00";
+    $f2 = $_POST['final']." 23:59:59";
+    $name = $_POST['name'];
+
+?>
+    <title>Document</title>
+</head>
+<body>
+
+    <form method="POST" action="generate.php">
+    <input type="date" name="inicio" >
+    <input type="date" name="final" >
+    <select name="name" id="">
+    <?php
+        while($datos = mysqli_fetch_array($select)){
+    ?>
+    <option value="<?php echo $datos['nombres'] ?>"> <?php echo $datos['nombres'] ?> </option>
+    <?php
+        }
+    ?>
+    </select>
+    <button type="submit">Generar reporte</button>
+    </form>
+    <?php 
+$resp = mysqli_query($con, "select v.idventa ,c.nombres,c.apellidos, p.nomprod , d.cantidad, v.fecha, d.precio from cliente c, producto p, venta v, detalle d
+WHERE d.idproducto = p.idproducto and d.idventa = v.idventa and v.idcliente = c.idcliente and v.fecha between '$f1' and '$f2' and c.nombres= '$name'");
+if ($resp) {
+    $xml = new DOMDocument("1.0");
+$xml->formatOutput=true;
+$fitness=$xml -> createElement("tienda");
+$xml -> appendChild($fitness);
+while($row=mysqli_fetch_array($resp)){
+    $reporte=$xml->createElement("reporte");
+    $fitness->appendChild($reporte);
+
+    $nombres=$xml->createElement("nombres", $row['nombres']);
+    $reporte->appendChild($nombres);
+
+    $apellidos=$xml ->createElement("apellidos", $row['apellidos']);
+    $reporte->appendChild($apellidos);
+
+    $nomprod=$xml -> createElement("nomprod", $row['nomprod']);
+    $reporte->appendChild($nomprod);
+
+    $cantidad=$xml -> createElement("cantidad", $row['cantidad']);
+    $reporte->appendChild($cantidad);
+
+    $fecha=$xml -> createElement("fecha", $row['fecha']);
+    $reporte->appendChild($fecha);
+
+    $preciototal=$xml -> createElement("precio", $row['precio']);
+    $reporte->appendChild($preciototal);
+}
+
+echo "<xmp>".$xml->saveXML()."</xmp>";
+$xml->save("report.xml");
+}else{
+    echo "Error...!";
+}
+
+?>
+</body>
+</html>
